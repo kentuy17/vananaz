88019e455fe194d48b84bf126dfb8d5250dfff84
Kenneth Camacho <camachokennethf@gmail.com>
2019-01-21 00:36:45 +0800

    no redux

--- App.js
+++ App.js
@@ -1,29 +1,85 @@
-import React, { Component } from 'react';
-import { AppRegistry, StyleSheet, Text, TextInput, View } from 'react-native';
+import React, { Component } from 'react'
+import { View, Image, Text, TouchableOpacity, TextInput, StyleSheet } from 'react-native'
+import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {text: ''};
-  }
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <TextInput
-                  style={{height: 40}}
-                  placeholder="Enter email: "
-        />
-      </View>
-    );
-  }
+class Inputs extends Component {
+   state = {
+      email: '',
+      password: ''
+   }
+   handleEmail = (text) => {
+      this.setState({ email: text })
+   }
+   handlePassword = (password) => {
+      this.setState({ password: password })
+   }
+   login = (email, pass) => {
+      alert('Login Success')
+   }
+   render() {
+      return (
+         <View style = {styles.container}>
+            <Image style = {styles.logo}
+            source={require('./Logo.png')} />
+
+            <TextInput style = {styles.input}
+               placeholder = "Input email address"
+               autoCapitalize = "none"
+               onChangeText = {this.handleEmail}/>
+            
+            <TextInput style = {styles.input}
+               placeholder = "Input password"
+               autoCapitalize = "none"
+               secureTextEntry = {true}
+               onChangeText = {this.handlePassword}/>
+            
+            <TouchableOpacity
+               style = {styles.submitButton}
+               onPress = {
+                  () => this.login(this.state.email, this.state.password)
+               }>
+               <Text style = {styles.submitButtonText}> Sign in </Text>
+            </TouchableOpacity>
+         </View>
+      )
+   }
 }
+export default Inputs
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+   container: {
+      paddingTop: 2,    
+      height: '100%',
+      justifyContent: 'center',
+      alignItems: 'center'
+   },
+   logo: {
+      width: 303,
+      height: 207,
+   },
+   input: {
+      margin: 15,
+      height: 40,
+      width: '90%',
+      borderColor: '#7a42f4',
+      borderWidth: 1,
+      padding: 10,
+      borderRadius: 5,
+      //placeholderStyle: {style: 'italic'},
+   },
+   submitButton: {
+      backgroundColor: '#7a42f4',
+      padding: 10,
+      margin: 15,
+      height: 40,
+      width: '90%',
+      alignText: 'center',
+      borderRadius: 5,
+      justifyContent: 'center',
+      alignItems: 'center',
+   },
+   submitButtonText:{
+      color: 'white',
+      fontWeight: 'bold'
+   }
+})
